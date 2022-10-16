34a05e46b89d601da4a7eaee1b80050048b6deb5
Luc Ho <hotriluc97@gmail.com>
2022-10-16 22:58:33 +0200

    adding animation to header navigation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,7 +18,11 @@ function Header(): JSX.Element {
   }, [currentTheme]);
 
   return (
-    <HeaderNav>
+    <HeaderNav
+      initial={{ y: -72, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
+    >
       <Container>
         <Flex spaceBetween noHeight>
           <Logo>
