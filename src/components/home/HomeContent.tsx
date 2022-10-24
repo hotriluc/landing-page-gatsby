2e8b726b4cbab8201fcf4be7b233a145b777326e
Luc Ho <hotriluc97@gmail.com>
2022-10-24 17:31:51 +0200

    animate on scroll to the section

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useAnimation } from 'framer-motion';
+import React, { useEffect } from 'react';
 import { Container } from '../../styles/Global.styles';
 import { Content, HomeContentSection } from '../../styles/Home.styles';
+import { useInView } from 'react-intersection-observer';
 
 const HomeContent = () => {
+  const animation = useAnimation();
+  const [contentRef, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: '-300px',
+  });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start('visible');
+    }
+  }, [animation, inView]);
+
   return (
-    <HomeContentSection>
+    <HomeContentSection
+      ref={contentRef}
+      animate={animation}
+      initial="hidden"
+      variants={{
+        visible: {
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] },
+        },
+        hidden: { opacity: 0, y: 72 },
+      }}
+    >
       <Container>
         <Content>
           Great stories don’t just happen— <br /> they need to be uncovered. And
